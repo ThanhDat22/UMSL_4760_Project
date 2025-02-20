ad1f09846c07f6db722bd1e61884b5e016eab6d4
ThanhDat22 <dat.nguyen2284@gmail.com>
2025-02-20 01:31:12 -0600

    edited Oss on 2025-02-20 16th

--- Project_2/thanh.2/Oss_Main.cpp
+++ Project_2/thanh.2/Oss_Main.cpp
@@ -2,26 +2,12 @@
 
 // Last edited by Thanh Dat Nguyen (tnrbf@umsystem.edu) on 2025-02-20
 
-// main.cpp is a source file that contains the main program
-
 #include "Oss.h"
 #include "Worker.h"
 
 #include <string.h>
 
 int main(int argc, char** argv) {
-
-    // Check if the program is running as worker
-    if((argc == 3) && (strcmp(argv[0], "./worker") == 0)) {
-        int seconds = atoi(argv[1]);
-        int nano = atoi(argv[2]);
-        Worker worker(seconds, nano);
-        worker.run();
-        return 0;
-    } 
-    
-    // Otherwise it is running as oss
-
     // Set the default values for the command line arguments
     int num_processes = 0;
     int max_simultaneous = 0;
@@ -33,5 +19,4 @@ int main(int argc, char** argv) {
     oss.parse_arguments(argc, argv); // Parse the command line arguments
     oss.run(); // Run the oss execution
     return 0;
-
 }
